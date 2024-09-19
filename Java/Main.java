f016eb2b6d75c25510b713d529e0958a1f126c99
divyasshree-BQ <124589840+divyasshree-BQ@users.noreply.github.com>
2024-09-19 17:38:56 +0530

    Add files via upload

--- Java/Main.java
+++ Java/Main.java
@@ -1,1 +1,66 @@
 package com.example;
+
+import org.apache.kafka.clients.CommonClientConfigs;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.config.SaslConfigs;
+import org.apache.kafka.common.serialization.ByteArrayDeserializer;
+import java.time.Duration;
+import javax.net.ssl.SSLContext;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.Properties;
+
+public class Main {
+    public static void main(String[] args) {
+        String topic = "tron.broadcasted.transactions";
+
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                "rpk0.bitquery.io:9093,rpk1.bitquery.io:9093,rpk2.bitquery.io:9093");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "trontest1-group-1");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
+        props.put(SaslConfigs.SASL_MECHANISM, "SCRAM-SHA-512");
+        props.put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_SSL");
+        props.put("ssl.endpoint.identification.algorithm", "");
+
+        props.put(SaslConfigs.SASL_JAAS_CONFIG,
+                "org.apache.kafka.common.security.scram.ScramLoginModule required username=\"ssss\" password=\"ssss\";");
+
+        props.put("ssl.keystore.location", "src/main/resources/keystore.jks");
+        props.put("ssl.keystore.password", "123456");
+        props.put("ssl.key.password", "123456");
+        props.put("ssl.truststore.location", "src/main/resources/clienttruststore.jks");
+        props.put("ssl.truststore.password", "truststorepassword");
+
+        KafkaConsumer<byte[], byte[]> consumer = new KafkaConsumer<>(props, new ByteArrayDeserializer(),
+                new ByteArrayDeserializer());
+
+        Runtime.getRuntime().addShutdownHook(new Thread(consumer::wakeup));
+
+        try {
+            consumer.subscribe(Collections.singletonList(topic));
+
+            while (true) {
+                ConsumerRecords<byte[], byte[]> records = consumer.poll(Duration.ofMillis(100));
+                for (ConsumerRecord<byte[], byte[]> record : records) {
+
+                    String value = new String(record.value());
+                    System.out.println(value);
+                }
+            }
+        } catch (WakeupException e) {
+            // Ignore for shutdown
+        } finally {
+            consumer.close();
+        }
+
+    }
+}
